21f1860a6128b3b2cd85ccf9d4adf9296541fe11
LayneKM <laynemikan@gmail.com>
2020-03-05 14:03:47 -0500

    Setup React project and Firebase hosting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <marquee><h1>Piano Password</h1></marquee>
+      Don't worry I'll take out the marquee
+    </React.Fragment>
+  );
+}
+
+export default App;
